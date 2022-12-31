877922d37da16a0a4700e80e9ba0441b3daa771a
SamnangGit <phengsamnanggit@gmail.com>
2022-12-31 22:00:59 +0700

    Add project files.

---
+++ WritingPlatform/Controllers/UsersController.cs
@@ -0,0 +1,216 @@
+﻿using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using WritingPlatform.Models;
+
+namespace WritingPlatform.Controllers
+{
+    public class UsersController : Controller
+    {
+        private WritingPlatformEntities db = new WritingPlatformEntities();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Details()
+        {
+            String[] btn = Request.Form.GetValues("btn");
+            System.Diagnostics.Debug.WriteLine("btn 0: " + btn[0]);
+            if (btn[0] == "Update")
+            {
+                User usr = (User)Session["user"];
+                return RedirectToAction("Edit", new { id = usr.Id });
+            }
+            else if (btn[0] == "Delete")
+            {
+                User usr = (User)Session["user"];
+                return RedirectToAction("Delete", new { id = usr.Id });
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        // GET: Users/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            System.Diagnostics.Debug.WriteLine("User Invoke");
+
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Username,Password,Email")] User user)
+        {
+            System.Diagnostics.Debug.WriteLine("User: " + user.Username);
+
+            if (ModelState.IsValid)
+            {
+                user.Password = GetSHA256Hash(user.Password);
+                user.Role = "user";
+                user.Status = true;
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Username,Email,Role")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                User u = (User)Session["user"];
+                User usr = db.Users.Find(u.Id);
+                user.Password = usr.Password;
+                user.Status = true;
+                usr.Username = user.Username;
+                usr.Email = user.Email;
+                usr.Role = user.Role;
+                System.Diagnostics.Debug.WriteLine("User: " + usr.Password);
+                System.Diagnostics.Debug.WriteLine("User: " + user.Username);
+
+                db.Entry(usr).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Details", new { id = usr.Id });
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();            
+        }
+
+        [HttpPost]
+        public ActionResult Login(String post)
+        {
+            String username = Request.Form["txtUsername"];
+            String password = Request.Form["pbPassword"];
+            System.Diagnostics.Debug.WriteLine("username: " + username);
+            System.Diagnostics.Debug.WriteLine("password: " + password);
+            password = GetSHA256Hash(password);
+            User user = db.Users.Where(u => u.Username == username && u.Password == password).FirstOrDefault();
+            if (user != null)
+            {
+                Session["user"] = user;
+                return RedirectToAction("UserDashboard", "Writing", new { id = user.Id });
+            }
+            else
+            {
+                ViewBag.Error = "Username or password is incorrect";
+                return View();
+            }
+
+        }
+
+        public string GetStringFromHash(byte[] hash)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                result.Append(hash[i].ToString("X2"));
+            }
+            return result.ToString();
+        }
+
+        public string GetSHA256Hash(string input)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(input);
+            byte[] hash = sha256.ComputeHash(bytes);
+            return GetStringFromHash(hash);
+        }
+    }
+}
---
+++ WritingPlatform/Controllers/WritingController.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WritingPlatform.Models;
+
+namespace WritingPlatform.Controllers
+{
+    public class WritingController : Controller
+    {
+        // GET: Writing
+        private WritingPlatformEntities db = new WritingPlatformEntities();
+        public List<String> genres = new List<string>();
+        public List<String> title_writings = new List<string>();
+        //public List<Writing> l_writing  = new List<Writing>();
+        public List<WritingDTO> l_writingDTO = new List<WritingDTO>();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard(int? id)
+        {
+            if (null != Request.Form.GetValues("btn"))
+            {
+                String[] btn = Request.Form.GetValues("btn");
+                System.Diagnostics.Debug.WriteLine("btn 0: " + btn[0]);
+                if (btn[0] == "Upload")
+                {
+                    return RedirectToAction("AddPublishWriting");
+                }
+                else if (btn[0] == "Compose")
+                {
+                    return RedirectToAction("PublishWriting");
+                }
+                return View();
+            }
+            else
+            {
+                User user = (User)Session["user"];
+                if (null == user)
+                {
+                    WritingDTO wDTO;
+                    foreach (Writing w in db.Writings)
+                    {
+                        wDTO = new WritingDTO();
+                        wDTO.Id = w.Id;
+                        wDTO.Username = w.User.Username;
+                        wDTO.Title = w.Title;
+                        wDTO.Type = w.Genre.Type;
+                        wDTO.Date = w.Date;
+                        wDTO.Path = w.Content;
+                        l_writingDTO.Add(wDTO);
+                        title_writings.Add(w.Title);
+                    }
+                    ViewBag.title_writings = title_writings;
+                    ViewData["l_writing"] = l_writingDTO;
+
+                    return View();
+                }
+                else
+                {
+                    WritingDTO wDTO;
+                    foreach (Writing w in db.Writings)
+                    {
+                        if(w.User_id == user.Id)
+                        {
+                            wDTO = new WritingDTO();
+                            wDTO.Id = w.Id;
+                            wDTO.Username = w.User.Username;
+                            wDTO.Title = w.Title;
+                            wDTO.Type = w.Genre.Type;
+                            wDTO.Date = w.Date;
+                            wDTO.Path = w.Content;
+                            l_writingDTO.Add(wDTO);
+                            title_writings.Add(w.Title);
+                        }
+
+                    }
+                    ViewBag.title_writings = title_writings;
+                    ViewData["l_writing"] = l_writingDTO;
+                    ViewBag.uid = user.Id;
+                    return View();
+                }
+            }
+        }
+
+        public ActionResult WritingDetail(int ? id)
+        {
+                if (id != null)
+                {
+                    String path = db.Writings.Find(id).Content;
+                    String mapPath = path.Replace("~", String.Empty);
+                    ViewBag.content = mapPath;
+                }
+
+                User user = (User)Session["user"];
+                if (null == user)
+                {
+                    return View();
+                }
+                else
+                {
+                    ViewBag.uid = user.Id;
+                    System.Diagnostics.Debug.WriteLine("user id: " + user.Id);
+                    return View();
+                }
+
+            }
+
+        public ActionResult WritingDetailAnonymous(int? id)
+        {
+            String path = db.Writings.Find(id).Content;
+            String mapPath = path.Replace("~", String.Empty);
+            ViewBag.content = mapPath;
+            return View();
+        }
+        public ActionResult PublishWriting()
+        {
+            User user = (User)Session["user"];
+            if (null == user)
+            {
+                return View();
+            }
+            else
+            {
+                ViewBag.uid = user.Id;
+                System.Diagnostics.Debug.WriteLine("user id: " + user.Id);
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AddPublishWriting()
+        {
+            User user = (User)Session["user"];
+            if (null == user)
+            {
+                foreach (Genre g in db.Genres)
+                {
+                    genres.Add(g.Type);
+                    System.Diagnostics.Debug.WriteLine("genre: " + g.Type);
+                }
+                ViewBag.genres = genres;
+                return View();
+            }
+            else
+            {
+                ViewBag.uid = user.Id;
+                foreach (Genre g in db.Genres)
+                {
+                    genres.Add(g.Type);
+                    System.Diagnostics.Debug.WriteLine("genre: " + g.Type);
+                }
+                ViewBag.genres = genres;
+                System.Diagnostics.Debug.WriteLine("user id: " + user.Id);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddPublishWriting(HttpPostedFileBase filename, [Bind(Include = "title,date,type")] Writing writing)
+        {
+            String type = Request.Form["selected-type"];
+            String title = Request.Form["title"];
+            String date = Request.Form["date"];
+
+            System.Diagnostics.Debug.WriteLine("type: " + type);
+            System.Diagnostics.Debug.WriteLine("title: " + title);
+            System.Diagnostics.Debug.WriteLine("date: " + date);
+
+            User user = (User)Session["user"];
+            string dir = Server.MapPath("~/Writing_Data/Papers/" + user.Username);
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            ViewBag.filename = Path.GetFileName(filename.FileName);
+            filename.SaveAs(Server.MapPath("~/Writing_Data/Papers/") + user.Username + "/" + filename.FileName);
+            ViewBag.FileList = Directory.GetFiles(Server.MapPath("~/Writing_Data/Papers/") + user.Username + "/");
+            System.Diagnostics.Debug.WriteLine("user name: " + user.Username);
+
+            writing.User_id = user.Id;
+            writing.Content = "~/Writing_Data/Papers/" + user.Username + "/" + filename.FileName;
+            writing.Date = DateTime.Now;
+            writing.Genre_id = db.Genres.Where(g => g.Type == type).FirstOrDefault().Id;
+            db.Writings.Add(writing);
+            db.SaveChanges();
+
+            foreach (Genre g in db.Genres)
+            {
+                genres.Add(g.Type);
+                System.Diagnostics.Debug.WriteLine("genre: " + g.Type);
+            }
+            ViewBag.genres = genres;
+            return View();
+        }
+
+        public ActionResult NonUserDashboard()
+        {
+                WritingDTO wDTO;
+                foreach (Writing w in db.Writings)
+                {
+                    wDTO = new WritingDTO();
+                    wDTO.Id = w.Id;
+                    wDTO.Username = w.User.Username;
+                    wDTO.Title = w.Title;
+                    wDTO.Type = w.Genre.Type;
+                    wDTO.Date = w.Date;
+                    wDTO.Path = w.Content;
+                    l_writingDTO.Add(wDTO);
+                    title_writings.Add(w.Title);
+                }
+                ViewBag.title_writings = title_writings;
+                ViewData["l_writing"] = l_writingDTO;
+
+                return View();
+
+        }
+        
+        public ActionResult UserDashboard(int? id)
+        {
+            User user = (User)Session["user"];
+            if (null == user)
+            {
+                WritingDTO wDTO;
+                foreach (Writing w in db.Writings)
+                {
+                    
+                    wDTO = new WritingDTO();
+                    wDTO.Id = w.Id;
+                    wDTO.Username = w.User.Username;
+                    wDTO.Title = w.Title;
+                    wDTO.Type = w.Genre.Type;
+                    wDTO.Date = w.Date;
+                    wDTO.Path = w.Content;
+                    l_writingDTO.Add(wDTO);
+                    title_writings.Add(w.Title);
+                }
+                ViewBag.title_writings = title_writings;
+                ViewData["l_writing"] = l_writingDTO;
+                return View();
+            }
+            else
+            {
+                WritingDTO wDTO;
+                foreach (Writing w in db.Writings)
+                {
+                    if (w.User_id == user.Id)
+                    {
+                        wDTO = new WritingDTO();
+                        wDTO.Id = w.Id;
+                        wDTO.Username = w.User.Username;
+                        wDTO.Title = w.Title;
+                        wDTO.Type = w.Genre.Type;
+                        wDTO.Date = w.Date;
+                        wDTO.Path = w.Content;
+                        l_writingDTO.Add(wDTO);
+                        title_writings.Add(w.Title);
+                    }
+ 
+                }
+                ViewBag.title_writings = title_writings;
+                ViewData["l_writing"] = l_writingDTO;
+                ViewBag.uid = user.Id;
+                return View(new { id = user.Id });
+            }
+        }
+    }
+
+    public class WritingDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public String Username { get; set; }
+        public String Type { get; set; }
+        public String Path { get; set; }
+    }
+}
